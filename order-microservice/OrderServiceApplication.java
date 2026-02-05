ba7ffb3003c7bb3e4537ce38aaf4f26c97b0c3a1
maxie <infomaxieandme@gmail.com>
2026-02-05 03:19:04 +0100

    updated the pipeline i removed the order service

--- order-microservice/OrderServiceApplication.java
+++
@@ -1,6 +0,0 @@
-@SpringBootApplication
-public class OrderServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
-    }
-}
