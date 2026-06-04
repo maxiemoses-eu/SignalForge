765a6622cfbc3079cb4561c65bd0e740229e4719
maxie <infomaxieandme@gmail.com>
2026-06-04 14:01:55 +0100

    added the order service

---
+++ order-microservice/src/test/java/com/signalforge/order/OrderSecurityTest.java
@@ -0,0 +1,11 @@
+package com.signalforge.order;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OrderSecurityTest {
+    @Test
+    void testOrderSanity() {
+        assertTrue(true);
+    }
+}
